467f8a17d6b523fef0b4c56a27c47129d21508fc
GhiveciA <ghiveci.adrian@yahoo.com>
2023-05-31 19:42:18 +0100

    Initial commit

---
+++ src/test/java/pages/BasePage.java
@@ -0,0 +1,12 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.sql.rowset.WebRowSet;
+
+public class BasePage {
+    private static final Logger Log = LoggerFactory.getLogger(BasePage.class);
+ public static WebDriver driver;
+}
