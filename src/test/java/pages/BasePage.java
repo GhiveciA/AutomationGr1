ff4d34e4bdfac998a92a9955b5bdea856f25ab66
GhiveciA <ghiveci.adrian@yahoo.com>
2023-06-19 21:52:59 +0100

    Modificat Register Test

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -1,12 +1,52 @@
 package pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.sql.rowset.WebRowSet;
-
 public class BasePage {
-    private static final Logger Log = LoggerFactory.getLogger(BasePage.class);
- public static WebDriver driver;
+    private static final Logger LOG = LoggerFactory.getLogger(BasePage.class);
+    public static WebDriver driver;
+
+    public static void setUp() {
+        LOG.info("Start test");
+        System.setProperty("webdriver.chrome.driver", "C:\\Webdrivers/chromedriver114.exe");
+        driver = new ChromeDriver();
+        String url = "https://demo.automationtesting.in/Index.html";
+        driver.get(url);
+
+        LOG.info("Open browser maximize");
+        driver.manage().window().maximize();
+    }
+
+    public static void tearDown() {
+        LOG.info("Close the browser");
+        driver.quit();
+    }
+
+    public static void sleep(long ms) {
+        try {
+            Thread.sleep(ms)
+
+            ;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String returnBaseUrl() {
+        return "https://demo.automationtesting.in/Index.html";
+
+    }
+
+    public static String getBaseUrl() {
+        String baseurl = returnBaseUrl();
+        if (baseurl != null) {
+            return baseurl.replace("Index.html", "");
+        }
+        return baseurl;
+    }
+
+
 }
---
+++ src/test/java/tests/RegisterTest/RegisterTest.java
@@ -0,0 +1,66 @@
+package tests.RegisterTest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.Test;
+import tests.BaseTest;
+
+import static pages.BasePage.*;
+
+
+public class RegisterTest extends BaseTest {
+    private static final Logger LOG = LoggerFactory.getLogger(RegisterTest.class);
+    private String newUrl = getBaseUrl() + "Register.html";
+    String fname = "Adrian";
+    String lname = "Ghiveci";
+    String adress = "64 Oakfield Road, Street";
+    String emailaddress = "ghiveci.adrian@yahoo.com";
+    String phone = "07496798394";
+
+    @Test
+    public void register() {
+        driver.get(newUrl);
+
+        LOG.info("Complete the First name and Last name");
+        registerPage.firstNameField(fname);
+        registerPage.lastNameField(lname);
+        sleep(3000);
+
+        LOG.info("Complete the Adress");
+        registerPage.adressField(adress);
+        sleep(3000);
+
+        LOG.info("Complete the Email Address");
+        registerPage.emailAddressField(emailaddress);
+        sleep(3000);
+
+        LOG.info("Complete the Phone");
+        registerPage.phoneField(phone)
+        ;
+        sleep(3000);
+
+        LOG.info("Click gender radio button");
+        registerPage.clickMaleGender();
+        sleep(3000);
+
+        LOG.info("Click gender radio button");
+        registerPage.clickFeMaleGender();
+        sleep(3000);
+
+        LOG.info("Click checkbox button");
+        registerPage.clickCricket();
+        sleep(3000);
+
+        LOG.info("Click checkbox button");
+        registerPage.clickMovies();
+        sleep(3000);
+
+        LOG.info("Click checkbox button");
+        registerPage.clickHockey();
+        sleep(3000);
+
+
+
+
+    }
+}
